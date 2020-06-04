e1b0f2dc1f339ead38dcaba665a7883f9c40fec5
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-06-04 11:13:07 -0700

    minimal setup.py
    to allow local pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='visual_behavior_glm',
+      version='0.0.0',
+      description='The funniest joke in the world',
+      url='https://github.com/AllenInstitute/visual_behavior_glm',
+      author='Alex Piet, Nick Ponvert',
+      author_email='alex.piet@alleninstitute.org',
+      license='Allen Institute',
+     )

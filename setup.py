0ba2f914e9a9dc1378c300877745cb09c9b0e2a1
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-06-04 11:14:29 -0700

    Fix template issue in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='visual_behavior_glm',
       version='0.0.0',
-      description='The funniest joke in the world',
+      description='GLM for Visual Behavior ophys data',
       url='https://github.com/AllenInstitute/visual_behavior_glm',
       author='Alex Piet, Nick Ponvert',
       author_email='alex.piet@alleninstitute.org',

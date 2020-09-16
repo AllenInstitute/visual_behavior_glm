ecdcf34f71ff77108af0efa66fe15cf5b787d1ef
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-09-16 11:33:09 -0700

    shell script for making a single movie

---
+++ scripts/make_glm_movie.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python make_GLM_movie.py\
+    --oeid 808621958\
+    --glm-version 6_L2_optimize_by_session\
+    --cell-id 817102506\
+    --start-frame 39400\
+    --end-frame 39500\
+    --frame-interval 1\
+    --fps 30\

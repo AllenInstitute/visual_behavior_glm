b242ec75ef20230f84bc938fa74fe5c0bfc290eb
Alex Piet <alexpiet@gmail.com>
2020-10-09 13:59:35 -0700

    typo

--- visual_behavior_glm/GLM_fit_tools.py
+++ visual_behavior_glm/GLM_fit_tools.py
@@ -996,7 +996,7 @@ def add_discrete_kernel_by_label(kernel_name,design, run_params,session,fit):
             event_times = session.dataset.rewards['timestamps'].values
         elif event == 'change':
             #event_times = session.dataset.trials.query('go')['change_time'].values # This method drops auto-rewarded changes
-            event_times = session.dataset.stimulus_presetations.query('change')['start_time'].values
+            event_times = session.dataset.stimulus_presentations.query('change')['start_time'].values
             event_times = event_times[~np.isnan(event_times)]
         elif event in ['hit', 'miss', 'false_alarm', 'correct_reject']:
             if event == 'hit': # Includes auto-rewarded changes as hits, since they include a reward. 

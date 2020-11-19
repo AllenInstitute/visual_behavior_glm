4921e3faedd587e8e2947553cd81280021a54fe7
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-11-19 13:31:45 -0800

    9b parameter changes

--- scripts/delete_rebuild_run_json.py
+++ scripts/delete_rebuild_run_json.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--version', 
         type=str, 
-        default='10a_L2_optimize_by_session',
+        default='10b_L2_optimize_by_session',
         metavar='model_version',
         help='model version'
     )
--- scripts/deploy_glm_fits.sh
+++ scripts/deploy_glm_fits.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source activate visual_behavior
-python deploy_glm_fits.py --version 10a_L2_optimize_by_session --env visual_behavior --src-path /home/dougo/code/visual_behavior_glm
+python deploy_glm_fits.py --version 10b_L2_optimize_by_session --env visual_behavior --src-path /home/dougo/code/visual_behavior_glm

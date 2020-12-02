b1b2c22b815d7f32c74e46768531cd00951d18fd
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-12-02 13:17:11 -0800

    import fix

--- visual_behavior_glm/GLM_visualization_tools.py
+++ visual_behavior_glm/GLM_visualization_tools.py
@@ -14,6 +14,7 @@ import time
 from tqdm import tqdm
 from matplotlib import animation, rc
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import gc
 from scipy import ndimage
 from scipy import stats

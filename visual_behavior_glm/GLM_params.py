d7c92ecb9f642654493169e6bf61a19fe61e0da7
Alex Piet <alexpiet@gmail.com>
2020-12-16 13:03:57 -0800

    adding events toggle in parameters

--- visual_behavior_glm/GLM_params.py
+++ visual_behavior_glm/GLM_params.py
@@ -188,7 +188,8 @@ def make_run_json(VERSION,label='',username=None, src_path=None, TESTING=False):
         'eye_transient_threshold': 0.5, # Threshold for excluding eye transients after blink removal
         'mean_center_inputs': True,     # If True, mean centers continuous inputs
         'unit_variance_inputs': True,   # If True, continuous inputs have unit variance
-        'max_run_speed': 100            # If 1, has no effect. Scales running speed to be O(1). 
+        'max_run_speed': 100,           # If 1, has no effect. Scales running speed to be O(1). 
+        'use_events': True              # If True, uses detected events, instead of df/f
     } 
     # Regularization parameter checks 
     a = run_params['L2_optimize_by_cell'] 

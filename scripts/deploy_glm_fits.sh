d826c695659ebb1cf90af85fad649a83a910bde1
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-12-21 16:59:44 -0800

    updating deploy script

--- scripts/deploy_glm_fits.sh
+++ scripts/deploy_glm_fits.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source activate visual_behavior
-python deploy_glm_fits.py --version 9a_L2_optimize_by_session --env visual_behavior --src-path /home/dougo/code/visual_behavior_glm
+python deploy_glm_fits.py --version 11a_L2_optimize_by_session --env visual_behavior --src-path /home/dougo/code/visual_behavior_glm

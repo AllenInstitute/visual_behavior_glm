8f8c20236e923fc4783873fc32541a6aec152c9d
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-12-22 22:30:17 -0800

    modified the script to generate GLM movies in a batch

--- scripts/make_movies_batch.py
+++ scripts/make_movies_batch.py
@@ -1,41 +1,50 @@
 from visual_behavior_glm.glm import GLM
 import visual_behavior_glm.GLM_analysis_tools as gat
-import visual_behavior_glm.GLM_visualization_tools as gvt
 
-import visual_behavior.utilities as vbu
-import visual_behavior.plotting as vbp
-import visual_behavior.data_access.loading as loading
-import visual_behavior.visualization.utils as vis_utils
+versions_to_compare = [
+    '10a_L2_optimize_by_session',
+    '11a_L2_optimize_by_session'
+]
 
-import seaborn as sns
-import matplotlib.pyplot as plt
+comparison_table = gat.get_glm_version_comparison_table(versions_to_compare)
 
-from multiprocessing import Pool
-
-def make_movie_of_top_cells(oeid):
-    print('making movies for oeid {}'.format(oeid))
-    version = '7_L2_optimize_by_session'
-    glm = GLM(int(oeid), version, use_previous_fit=True, log_results=False, log_weights=False)
-    glm_results = glm.results.sort_values(by=['Full__avg_cv_var_test'], ascending=False)
-    for cell_specimen_id in glm_results.iloc[:2].index.values:
-        movie = gvt.GLM_Movie(
-            glm,
-            cell_specimen_id = cell_specimen_id, 
-            start_frame = 30000,
-            end_frame = 35000,
-            frame_interval = 1,
-            destination_folder='/home/dougo/OneDrive/glm_movies',
-            fps = 15
-        )
-        movie.make_movie()
-
-experiment_table = loading.get_filtered_ophys_experiment_table()
-table_sample = experiment_table.sample(500, random_state = 1)
-
-for ophys_experiment_id in table_sample.index.values:
+def version_diff(row):
     try:
-        make_movie_of_top_cells(ophys_experiment_id)
-    except Exception as e:
-        print('failed on {}'.format(ophys_experiment_id))
-        print(e)
-        print(' ')
+        return (row['10a_L2_optimize_by_session'] - row['11a_L2_optimize_by_session'])/row['10a_L2_optimize_by_session']
+    except ZeroDivisionError:
+        return None
+comparison_table['version_diff'] = comparison_table.apply(version_diff, axis=1)
+
+big_droppers = (
+    comparison_table[
+        (comparison_table[versions_to_compare[0]] > 0.1)
+        &(comparison_table[versions_to_compare[1]] > 0)
+    ]
+    .dropna(subset=versions_to_compare)[versions_to_compare + ['version_diff','identifier','cre_line']]
+    .sort_values(by='version_diff', ascending=False)
+    .iloc[:20]
+)
+
+for idx, row in big_droppers.iterrows():
+    oeid, csid = row['identifier'].split('_')
+    for version in versions_to_compare:
+        try:
+            glm = GLM(
+                oeid, 
+                version, 
+                use_previous_fit=True,  # if True, uses cached fit if available (False by default)
+                log_results=False, # if True, logs fit results to mongo database (True by default)
+                log_weights=False, # if True, logs weights to mongo database (True by default)
+                NO_DROPOUTS=False, # if True, does not perform dropout analysis (False by default)
+                TESTING=False, # if True, fits only the first 6 cells in the experiment (False by default)
+            )
+
+            glm.movie(
+                cell_specimen_id=csid,
+                action='make_movie',
+                frame_interval=50,
+                fps=5,
+                destination_folder = '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/ophys_glm/events_dff_comparison_videos'
+            )
+        except:
+            pass

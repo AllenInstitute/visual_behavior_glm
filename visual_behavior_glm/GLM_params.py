d01630ff92b3b3cb25860f800257ed1f556f497c
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-05-26 10:56:49 -0700

    assertion fix ;

--- visual_behavior_glm/GLM_params.py
+++ visual_behavior_glm/GLM_params.py
@@ -192,7 +192,7 @@ def make_run_json(VERSION,label='',username=None, src_path=None, TESTING=False):
         'mean_center_inputs': True,     # If True, mean centers continuous inputs
         'unit_variance_inputs': True,   # If True, continuous inputs have unit variance
         'max_run_speed': 100,           # If 1, has no effect. Scales running speed to be O(1). 
-        'use_events': False             # If True, use detected events. If False, use raw deltaF/F 
+        'use_events': True             # If True, use detected events. If False, use raw deltaF/F 
     } 
     # Regularization parameter checks 
     a = run_params['L2_optimize_by_cell'] 
@@ -217,7 +217,7 @@ def make_run_json(VERSION,label='',username=None, src_path=None, TESTING=False):
     
     # Check Engagement split parameters
     if run_params['split_on_engagement']:
-        assert (run_params['engagement_preference'] is 'engaged') or (run_params['engagement_preference'] is 'disengaged'), "Splitting on engagement, preference must be 'engaged' or 'disengaged'"
+        assert (run_params['engagement_preference'] == 'engaged') or (run_params['engagement_preference'] == 'disengaged'), "Splitting on engagement, preference must be 'engaged' or 'disengaged'"
     elif not run_params['split_on_engagement']: 
         assert run_params['engagement_preference'] is None, "Not splitting on engagement, engagement preference must be None"
     assert run_params['min_engaged_duration'] >=0, "Must define a minimum interval for the preferred engagement state"

2763e3f0f014b668a5e474f3359fb620326987cf
Alex Piet <alexpiet@gmail.com>
2021-06-25 23:24:13 -0700

    set up distribution

--- visual_behavior_glm/GLM_transition.py
+++ visual_behavior_glm/GLM_transition.py
@@ -12,7 +12,13 @@ K = 5
 - am I computing the estimate correctly? Right math, right code?
     should use np.dot(), not *
     order of operations
-
+- filter by cre line
+- Need to incorporate NaNs in transition matrices
+- Need to flexibly incorporate multiple estimates
+- Add NaNs to distribution
+- Add error bars to measured distribution
+- Add error bars to estimated distribution
+- remove tracked cells from estimate?
 '''
 
 def load_table():
@@ -42,6 +48,8 @@ def build_estimates(normTs):
                 normTs[str(s)+str(e)] = normTs[str(s)+str(e-1)]*normTs[str(s-1)+str(e)]
             else:
                 # can we rely on subestimates already been computed? depends on order
+                normTs[str(s)+str(e)] = normTs[str(s)+str(e-1)]*normTs[str(s-1)+str(e)]
+
 
 def make_matrix(df, sessions=[1,3]):
     # assert sessions is length 2
@@ -84,23 +92,90 @@ def sanity_check_matrix():
     T[0,2] = 3 
     # makes a everything stay in its cluster, except for three cells that go from
     # cluster 2 to cluster 0
-    plot_matrix(T,[1,2], k=2)
-    
-def plot_matrix(T,sessions,k=None):
+    return T
+
+def plot_sanity_check():
+    T = sanity_check_matrix()
+    normT = normalize_matrix(T)
+    plot_matrix(T,[1,2], k=2,normalized=False)
+    plot_matrix(normT,[1,2], k=2)   
+ 
+def plot_matrix(T,sessions,k=None,normalized=True,ax=None):
     if k is None:
         k=K
-    plt.figure()
-    im = plt.imshow(T,cmap='Blues')
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    if normalized:
+        cmap = 'Blues'
+    else:
+        cmap = 'Reds'
+    im = ax.imshow(T,cmap=cmap)
     # triple check axis order, and value
-    plt.ylabel('Session '+str(sessions[1]),fontsize=16)
-    plt.xlabel('Session '+str(sessions[0]),fontsize=16)
+    ax.set_ylabel('Session '+str(sessions[1]),fontsize=16)
+    ax.set_xlabel('Session '+str(sessions[0]),fontsize=16)
     labels = [str(x) for x in list(range(0,k))]+['NaN']
-    plt.xticks(range(0,k+1),labels, fontsize=14)
-    plt.yticks(range(0,k+1),labels, fontsize=14)
-    cbar = plt.colorbar(im)
+    ax.set_xticks(range(0,k+1))
+    ax.set_xticklabels(labels, fontsize=14)
+    ax.set_yticks(range(0,k+1))
+    ax.set_yticklabels(labels, fontsize=14)
+    cbar = plt.colorbar(im,ax=ax)
     cbar.ax.tick_params(labelsize=14)
-    cbar.set_label('Prob(Session '+str(sessions[1])+'| Session '+str(sessions[0])+')',fontsize=14)
+    if normalized:
+        cbar.set_label('Prob('+str(sessions[1])+'|'+str(sessions[0])+')',fontsize=14)
+    else:
+         cbar.set_label('Count '+str(sessions[0])+'-> '+str(sessions[1])+'',fontsize=14)   
     plt.tight_layout()
     
+def plot_normalization(Ts, normTs, sessions):
+    key = ''.join([str(x) for x in sessions])
+    fig, axes = plt.subplots(1,2,figsize=(8,3))
+    plot_matrix(Ts[key],sessions, normalized=False, ax=axes[0])
+    plot_matrix(normTs[key], sessions, ax=axes[1])
+    plt.tight_layout()
+    
+def compute_distribution(df,session):
+    session_dist = df.query('session_number == @session').groupby('K_cluster').count()[['cell_specimen_id']]
+    cell_count = len(df.cell_specimen_id.unique()) - session_dist['cell_specimen_id'].sum()
+    session_dist.at[K] = [cell_count]
+    session_dist['normalized'] = session_dist['cell_specimen_id']/sum(session_dist['cell_specimen_id'])
+    return session_dist
+
+def compute_full_estimate(df,T, in_session,out_session):
+    in_dist = compute_distribution(df,in_session)
+    out_dist= compute_distribution(df, out_session)
+    out_dist= compute_estimate(T,in_dist, out_dist)
+    plot_estimate_distribution(out_dist)
+
+def compute_estimate(T,in_session_dist,out_session_dist):
+    out_session_dist['estimate_count'] = np.dot(T,in_session_dist['cell_specimen_id'])
+    out_session_dist['estimate_normalized'] = out_session_dist['estimate_count']/sum(out_session_dist['estimate_count'])
+    return out_session_dist
+    
+def plot_estimate_distribution(session_dist):
+    plt.figure()
+    ax = plt.gca()
+    x = np.arange(0,K+1)
+    width=0.35
+    rects1 = ax.bar(x-width/2,session_dist['normalized'], width, label='Measured')
+    rects2 = ax.bar(x+width/2,session_dist['estimate_normalized'], width, label='Estimate')
+    ax.set_ylabel('Fraction',fontsize=16)
+    ax.set_xlabel('Cluster', fontsize=16)
+    ax.set_ylim(0,1)
+    ax.tick_params(axis='both',labelsize=14)
+    ax.legend()
+    plt.tight_layout()
+
+def plot_distribution(session_dist,ax=None,key='normalized'):
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    ax.bar(session_dist.index, session_dist[key])
+    ax.set_ylabel('Fraction',fontsize=16)
+    ax.set_xlabel('Cluster', fontsize=16)
+    ax.set_ylim(0,1)
+    ax.tick_params(axis='both',labelsize=14)
+    ax.set_title(key)
+
 
 

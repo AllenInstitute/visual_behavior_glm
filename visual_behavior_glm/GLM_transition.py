d78482c73d5d4d331d8f7d86c121e676782f1d33
Alex Piet <alexpiet@gmail.com>
2021-06-28 20:50:23 -0700

    rebuild with all cells, accounting for cree lines, and building pivot tables

--- visual_behavior_glm/GLM_transition.py
+++ visual_behavior_glm/GLM_transition.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from statsmodels.stats.proportion import multinomial_proportions_confint as mpc
 K = 5
 
 '''
@@ -26,6 +26,116 @@ def load_table():
     df = pd.read_hdf(filepath)
     return df
 
+def clean_table(df):
+    df['identifier'] = [str(x[0])+'_'+str(x[1]) for x in zip(df['cell_specimen_id'], df['session_number'])]
+    df = df.drop_duplicates(subset='identifier').copy()
+    return df
+
+def build_pivot_table(cre='all'):
+    df = load_table()
+    if cre != 'all':
+        df = df.query('cre_line == @cre').copy()
+    df = clean_table(df)
+    df_pivot = pd.pivot_table(df, index='cell_specimen_id',columns='session_number',values='K_cluster')
+    df_pivot = df_pivot.rename(columns={1:'1',2:'2',3:'3',4:'4',5:'5',6:'6'})
+    df_pivot['tracked_13'] = df_pivot[['1','3']].isnull().sum(axis=1) ==0
+    df_pivot['tracked_12'] = df_pivot[['1','2']].isnull().sum(axis=1) ==0
+    df_pivot['tracked_34'] = df_pivot[['3','4']].isnull().sum(axis=1) ==0
+    return df_pivot
+
+def compute_distribution(df_pivot, session,session_dists={},dropna=False):
+    session_dists[str(session)] = df_pivot[str(session)].value_counts(dropna=dropna)
+    for val in range(0,K):
+        if val not in session_dists[str(session)].index:
+            session_dists[str(session)].loc[val] = 0
+        if (not dropna) & (np.nan not in session_dists[str(session)].index):
+            session_dists[str(session)].loc[np.nan] = 0
+    session_dists[str(session)] = session_dists[str(session)].sort_index(na_position='first')
+    session_dists['normalized_'+str(session)] = session_dists[str(session)]/sum(session_dists[str(session)])
+    session_dists['ci_'+str(session)] = mpc(session_dists[str(session)])
+    return session_dists
+
+def compute_all_distributions(df_pivot):
+    session_dists = {}
+    session_dists_no_nans = {}
+    for i in range(1,7):
+        session_dists = compute_distribution(df_pivot, i, session_dists = session_dists)
+        session_dists_no_nans = compute_distribution(df_pivot, i, session_dists = session_dists_no_nans, dropna=True)
+    return session_dists, session_dists_no_nans 
+
+def check_distributions(cre='all'):
+    df_pivot = build_pivot_table(cre=cre)
+    session_dists, session_dists_no_nans = compute_all_distributions(df_pivot)
+    session_dists_13, session_dists_no_nans_13 = compute_all_distributions(df_pivot.query('tracked_13'))
+    session_dists_12, session_dists_no_nans_12 = compute_all_distributions(df_pivot.query('tracked_12'))
+    session_dists_34, session_dists_no_nans_34 = compute_all_distributions(df_pivot.query('tracked_34'))
+    session_dists_n13, session_dists_no_nans_n13 = compute_all_distributions(df_pivot.query('not tracked_13'))
+    session_dists_n12, session_dists_no_nans_n12 = compute_all_distributions(df_pivot.query('not tracked_12'))
+    session_dists_n34, session_dists_no_nans_n34 = compute_all_distributions(df_pivot.query('not tracked_34'))
+
+    ## Result 1, are clusters different across sessions?
+    plot_distribution_by_session(session_dists, [str(x) for x in range(1,7)],title=cre)
+    ## Result 2, novelty clusters are different without nans!
+    plot_distribution_by_session(session_dists_no_nans, [str(x) for x in range(1,7)],dropna=True,title=cre)
+
+    #plot_distribution_by_group([session_dists, session_dists_13],1,labels=['all','tracked 1,3'])
+    plot_distribution_by_group([session_dists_no_nans, session_dists_no_nans_13, session_dists_no_nans_n13],1,labels=['all','tracked 1,3','not tracked 1,3'],dropna=True,title='Session 1')
+    plot_distribution_by_group([session_dists_no_nans, session_dists_no_nans_13, session_dists_no_nans_n13],3,labels=['all','tracked 1,3','not tracked 1,3'],dropna=True,title='Session 3')
+
+
+    #plot_distribution_by_group([session_dists, session_dists_12],1,labels=['all','tracked 1,2'])
+    plot_distribution_by_group([session_dists_no_nans, session_dists_no_nans_12,session_dists_no_nans_n12],1,labels=['all','tracked 1,2','not tracked 1,2'],dropna=True,title='Session 1')
+    plot_distribution_by_group([session_dists_no_nans, session_dists_no_nans_12,session_dists_no_nans_n12],2,labels=['all','tracked 1,2','not tracked 1,2'],dropna=True,title='Session 2')
+
+    plot_distribution_by_group([session_dists_no_nans, session_dists_no_nans_34,session_dists_no_nans_n34],3,labels=['all','tracked 3,4','not tracked 3,4'],dropna=True,title='Session 3')
+    plot_distribution_by_group([session_dists_no_nans, session_dists_no_nans_34,session_dists_no_nans_n34],4,labels=['all','tracked 3,4','not tracked 3,4'],dropna=True,title='Session 4')
+
+
+def plot_distribution_by_session(session_dists,sessions,dropna=False,title=''):
+    plt.figure()
+    ax = plt.gca()
+    if dropna:
+        x = np.arange(0,K)
+    else:
+        x = np.arange(0,K+1)
+    width=0.7/len(sessions)
+    for dex,s in enumerate(sessions):
+        xvec = x+width*(dex+1)-.35
+        ax.bar(xvec,session_dists['normalized_'+str(s)], width, label=s)
+        ax.plot(np.array([xvec,xvec]),session_dists['ci_'+str(s)].T,'k-')
+    ax.set_ylabel('Fraction',fontsize=16)
+    ax.set_xlabel('Cluster', fontsize=16)
+    ax.set_ylim(0,1)
+    ax.set_xticks(x)
+    ax.set_xticklabels(session_dists['normalized_1'].index.values)
+    ax.tick_params(axis='both',labelsize=14)
+    ax.legend()
+    ax.set_title(title)
+    plt.tight_layout()
+
+def plot_distribution_by_group(session_dists_lists, session, dropna=False, title='',labels=[]):
+    plt.figure()
+    ax = plt.gca()
+    if dropna:
+        x = np.arange(0,K)
+    else:
+        x = np.arange(0,K+1)
+    width=0.7/len(session_dists_lists)
+    for dex,s in enumerate(session_dists_lists):
+        xvec = x+width*(dex+1)-.35
+        ax.bar(xvec,s['normalized_'+str(session)], width, label=labels[dex])
+        ax.plot(np.array([xvec,xvec]),s['ci_'+str(session)].T,'k-')
+    ax.set_ylabel('Fraction',fontsize=16)
+    ax.set_xlabel('Cluster', fontsize=16)
+    ax.set_ylim(0,1)
+    ax.set_xticks(x)
+    ax.set_xticklabels(session_dists_lists[0]['normalized_1'].index.values)
+    ax.tick_params(axis='both',labelsize=14)
+    ax.legend()
+    ax.set_title(title)
+    plt.tight_layout()
+
+#####################
 def make_all(df):
     Ts = {}
     sessions = [1,2,3,4,5,6]
@@ -134,7 +244,7 @@ def plot_normalization(Ts, normTs, sessions):
     plot_matrix(normTs[key], sessions, ax=axes[1])
     plt.tight_layout()
     
-def compute_distribution(df,session):
+def OLD_compute_distribution(df,session):
     session_dist = df.query('session_number == @session').groupby('K_cluster').count()[['cell_specimen_id']]
     cell_count = len(df.cell_specimen_id.unique()) - session_dist['cell_specimen_id'].sum()
     session_dist.at[K] = [cell_count]
@@ -152,30 +262,5 @@ def compute_estimate(T,in_session_dist,out_session_dist):
     out_session_dist['estimate_normalized'] = out_session_dist['estimate_count']/sum(out_session_dist['estimate_count'])
     return out_session_dist
     
-def plot_estimate_distribution(session_dist):
-    plt.figure()
-    ax = plt.gca()
-    x = np.arange(0,K+1)
-    width=0.35
-    rects1 = ax.bar(x-width/2,session_dist['normalized'], width, label='Measured')
-    rects2 = ax.bar(x+width/2,session_dist['estimate_normalized'], width, label='Estimate')
-    ax.set_ylabel('Fraction',fontsize=16)
-    ax.set_xlabel('Cluster', fontsize=16)
-    ax.set_ylim(0,1)
-    ax.tick_params(axis='both',labelsize=14)
-    ax.legend()
-    plt.tight_layout()
-
-def plot_distribution(session_dist,ax=None,key='normalized'):
-    if ax is None:
-        plt.figure()
-        ax = plt.gca()
-    ax.bar(session_dist.index, session_dist[key])
-    ax.set_ylabel('Fraction',fontsize=16)
-    ax.set_xlabel('Cluster', fontsize=16)
-    ax.set_ylim(0,1)
-    ax.tick_params(axis='both',labelsize=14)
-    ax.set_title(key)
-
 
 

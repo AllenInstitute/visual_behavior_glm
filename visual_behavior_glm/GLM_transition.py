753edd9804c30eea2f9eb1c91cd024b70bb88da1
Alex Piet <alexpiet@gmail.com>
2021-06-30 14:16:56 -0700

    transition and distribution analysis finalized

--- visual_behavior_glm/GLM_transition.py
+++ visual_behavior_glm/GLM_transition.py
@@ -5,22 +5,11 @@ from statsmodels.stats.proportion import multinomial_proportions_confint as mpc
 K = 5
 
 '''
-- check novel only familar only
 - ensure cell shows up in each container
 - if more than one session type, just taking the first
-- how to deal with NaNs, there could always be more cells "lurking"
-    - Can I just assume that all cells show up at least once?
-    - repeat with and without nan cells?
-- am I computing the estimate correctly? Right math, right code?
-    should use np.dot(), not *
-    order of operations
-- filter by cre line
-- Need to incorporate NaNs in transition matrices
-- Need to flexibly incorporate multiple estimates
-- Add NaNs to distribution
-- Add error bars to measured distribution
-- Add error bars to estimated distribution
-- remove tracked cells from estimate?
+- Document
+- Check estimate ordering of indexes in estimate construction
+- update transition matrix
 '''
 
 FIGURE_DIR = '/allen/programs/braintv/workgroups/nc-ophys/alex.piet/GLM/clustering_figs/'
@@ -45,6 +34,9 @@ def build_pivot_table(cre='all'):
     df_pivot['tracked_13'] = df_pivot[['1','3']].isnull().sum(axis=1) ==0
     df_pivot['tracked_12'] = df_pivot[['1','2']].isnull().sum(axis=1) ==0
     df_pivot['tracked_34'] = df_pivot[['3','4']].isnull().sum(axis=1) ==0
+    df_pivot['tracked_23'] = df_pivot[['2','3']].isnull().sum(axis=1) ==0
+    df_pivot['tracked_45'] = df_pivot[['4','5']].isnull().sum(axis=1) ==0
+    df_pivot['tracked_56'] = df_pivot[['5','6']].isnull().sum(axis=1) ==0
     df_pivot['never_tracked']=df_pivot[['1','2','3','4','5','6']].isnull().sum(axis=1) ==5
     df_pivot['full_tracked']=df_pivot[['1','2','3','4','5','6']].isnull().sum(axis=1) ==0
     df_pivot['full_novel_tracked']= (df_pivot[['4','5','6']].isnull().sum(axis=1)==0)&(df_pivot[['1','2','3']].isnull().sum(axis=1)==3)
@@ -114,10 +106,21 @@ def example_transition(cre='Slc17a7-IRES2-Cre',sessions=['3','4']):
 
 def build_estimate(df_pivot, sessions, session_dists):
     T = build_transition_matrix(df_pivot, sessions)
+    T = clean_transition_matrix(T)
     session_dists['estimate_'+str(sessions[0])+'_to_'+str(sessions[1])] = np.dot(normalize_matrix(T),session_dists['normalized_'+str(sessions[0])])
     session_dists['estimate_ci_'+str(sessions[0])+'_to_'+str(sessions[1])] = np.dot(normalize_matrix(T),session_dists['ci_'+str(sessions[0])])
     return session_dists
-    
+
+def clean_transition_matrix(T,verbose=False):
+    cleaned = False
+    for c in T.columns:
+        if T[c].sum() == 0:
+            T[c] = 1
+            cleaned = True
+    if cleaned & verbose:
+        print('Cleaned transition matrix')
+    return T   
+ 
 def build_all_estimates(session_dists,df_pivot):
     sessions = [1,2,3,4,5,6]
     for sdex, s in enumerate(sessions):
@@ -125,11 +128,58 @@ def build_all_estimates(session_dists,df_pivot):
             session_dists = build_estimate(df_pivot, [str(e),str(s)], session_dists)
     return session_dists
 
+def plot_estimate(cre='all',transition_query='',in_dist_query='',out_dist_query='',sessions=['3','4']):
+    
+    # Filter subsets of cells for each step
+    labels = ['Measured','Estimate']
+    if transition_query == '':
+        df_pivot_transition = build_pivot_table(cre=cre)
+        labels[1] += ' \nTransition  = all cells tracked '+str(sessions[0])+','+str(sessions[1])
+    else:
+        df_pivot_transition = build_pivot_table(cre=cre).query(transition_query)
+        labels[1] += ' \nTransition  = '+transition_query
+    if in_dist_query == '':
+        df_pivot_in_dist    = build_pivot_table(cre=cre)
+        labels[1] += ' \nInput Dist  = all cells in '+str(sessions[0])
+    else:
+        df_pivot_in_dist    = build_pivot_table(cre=cre).query(in_dist_query)
+        labels[1] += ' \nInput Dist  = '+in_dist_query
+    if out_dist_query == '':
+        df_pivot_out_dist   = build_pivot_table(cre=cre)
+        labels[0] += ' \nOutput Dist = all cells in '+str(sessions[1])+'\n'
+    else:
+        df_pivot_out_dist   = build_pivot_table(cre=cre).query(out_dist_query)
+        labels[0] += ' \nOutput Dist = '+out_dist_query +'\n'
+
+    # Build distributions
+    session_dists_with_nans, in_session_dists = compute_all_distributions(df_pivot_in_dist)
+    est_session_dists = build_all_estimates(in_session_dists, df_pivot_transition)
+    out_session_dists_with_nans, out_session_dists = compute_all_distributions(df_pivot_out_dist)
+
+    title = 'Estimating Session '+str(sessions[1])+' from Session '+str(sessions[0])
+    savefile = 'estimate_session_'+str(sessions[1])+'_from_session_'+str(sessions[0])+'_'+cre+'_transition_'+transition_query+'_input_'+in_dist_query+'_output_'+out_dist_query+'.png'
+    plot_distribution_estimate(out_session_dists, est_session_dists, sessions,title=title,labels=labels,savefile=savefile)
+
 def check_estimates(cre='all'):
-    df_pivot = build_pivot_table(cre=cre)
-    session_dists_with_nans, session_dists = compute_all_distributions(df_pivot)
-    session_dists = build_all_estimates(session_dists, df_pivot)
-    return session_dists 
+    plot_estimate(cre=cre, transition_query='full_tracked',in_dist_query='full_familiar_tracked', out_dist_query='full_novel_tracked', sessions=['3','4'])
+    plot_estimate(cre=cre, transition_query='full_tracked',in_dist_query='partial_familiar_tracked', out_dist_query='partial_novel_tracked', sessions=['3','4'])
+
+    plot_estimate(cre=cre, in_dist_query='full_familiar_tracked', out_dist_query='full_novel_tracked', sessions=['3','4'])
+    plot_estimate(cre=cre, in_dist_query='partial_familiar_tracked', out_dist_query='partial_novel_tracked', sessions=['3','4'])
+
+    plot_estimate(cre=cre, in_dist_query='not tracked_34', out_dist_query='not tracked_34', sessions=['3','4'])
+    plot_estimate(cre=cre, sessions=['3','4'])
+
+    plot_estimate(cre=cre, sessions=['1','2'])
+    plot_estimate(cre=cre, sessions=['2','3'])
+    plot_estimate(cre=cre, sessions=['4','5'])
+    plot_estimate(cre=cre, sessions=['5','6'])
+
+    plot_estimate(cre=cre, in_dist_query='not tracked_12', out_dist_query='not tracked_12', sessions=['1','2'])
+    plot_estimate(cre=cre, in_dist_query='not tracked_23', out_dist_query='not tracked_23', sessions=['2','3'])
+    plot_estimate(cre=cre, in_dist_query='not tracked_45', out_dist_query='not tracked_45', sessions=['4','5'])
+    plot_estimate(cre=cre, in_dist_query='not tracked_56', out_dist_query='not tracked_56', sessions=['5','6'])
+
 
 def check_transitions(cre='all'):
     df_pivot = build_pivot_table(cre=cre)
@@ -315,89 +365,36 @@ def plot_normalization(T, sessions):
     plt.tight_layout()
 
 
-def plot_distribution_estimate(session_dists, sessions,title='',labels=[]):
+def plot_distribution_estimate(measured_session_dists, estimate_session_dists, sessions,title='',labels=[],savefile=None):
     plt.figure()
     ax = plt.gca()
     x = np.arange(0,K)
     width=0.7/2
     xvec = x+width*(0+1)-.35
-    s = session_dists
-    ax.bar(xvec,s['normalized_'+str(sessions[1])], width, label='Measured')
-    ax.plot(np.array([xvec,xvec]),s['ci_'+str(sessions[1])].T,'k-')
+    ms = measured_session_dists
+    es = estimate_session_dists
+    ax.bar(xvec,ms['normalized_'+str(sessions[1])], width, label=labels[0])
+    ax.plot(np.array([xvec,xvec]),ms['ci_'+str(sessions[1])].T,'k-')
 
     xvec = x+width*(1+1)-.35
-    ax.bar(xvec,s['estimate_'+str(sessions[0])+'_to_'+str(sessions[1])], width, label='Estimate')
-    ax.plot(np.array([xvec,xvec]),s['estimate_ci_'+str(sessions[0])+'_to_'+str(sessions[1])].T,'k-')
+    ax.bar(xvec,es['estimate_'+str(sessions[0])+'_to_'+str(sessions[1])], width, label=labels[1])
+    ax.plot(np.array([xvec,xvec]),es['estimate_ci_'+str(sessions[0])+'_to_'+str(sessions[1])].T,'k-')
 
 
     ax.set_ylabel('Fraction',fontsize=16)
     ax.set_xlabel('Cluster', fontsize=16)
     ax.set_ylim(0,1)
     ax.set_xticks(x)
-    ax.set_xticklabels(session_dists['normalized_1'].index.values)
+    ax.set_xticklabels(measured_session_dists['normalized_1'].index.values)
     ax.tick_params(axis='both',labelsize=14)
     ax.legend()
     ax.set_title(title)
     plt.tight_layout()
-    #plt.savefig(FIGURE_DIR+title+'_'+str(session)+'_'+labels[1]+'.png')
-
-#####################
-#def make_all(df):
-#    Ts = {}
-#    sessions = [1,2,3,4,5,6]
-#    for sdex, s in enumerate(sessions):
-#        for edex, e in enumerate(sessions[sdex+1:]):
-#            print('Computing '+str(s)+' '+str(e))
-#            Ts[str(s)+str(e)] = make_matrix(df, sessions=[s,e])
-#    normTs = {}
-#    for m in Ts:
-#        normTs[m] = normalize_matrix(Ts[m])
-#    return Ts, normTs
-
-#def build_estimates(normTs):
-#    sessions = [1,2,3,4,5,6]
-#    for sdex, s in enumerate(sessions):
-#        for edex, e in enumerate(sessions[sdex+2:]):
-#            print('Computing '+str(s)+' '+str(e))
-#            if e-s ==2:
-#                # we shouldn't need to renormalize
-#                normTs[str(s)+str(e)] = normTs[str(s)+str(e-1)]*normTs[str(s-1)+str(e)]
-#            else:
-#                # can we rely on subestimates already been computed? depends on order
-#                normTs[str(s)+str(e)] = normTs[str(s)+str(e-1)]*normTs[str(s-1)+str(e)]
+    if savefile is not None:
+        plt.savefig(FIGURE_DIR+savefile)
 
 
-#def make_matrix(df, sessions=[1,3]):
-#    # assert sessions is length 2
-#
-#    # Make empty matrix
-#    T = np.empty((K+1,K+1))
-#    T[:] = 0 
-#
-#
-#    dfs = df.query('session_number in @sessions')
-#    cells = dfs.cell_specimen_id.unique()
-#
-#    # So slow!
-#    # Can probably speed up by making table of cells, columns as sessions, then doing a groupby
-#    for cell in cells:
-#        sA = dfs.query('(cell_specimen_id == @cell) & (session_number == @sessions[0])')
-#        sB = dfs.query('(cell_specimen_id == @cell) & (session_number == @sessions[1])')
-#        if len(sA) == 0:
-#            clusterA = K
-#        else:
-#            clusterA = sA.iloc[0]['K_cluster']
-#        if len(sB) == 0:
-#            clusterB = K
-#        else:
-#            clusterB = sB.iloc[0]['K_cluster']
-#        T[clusterB,clusterA] +=1
-#        # T(2nd session, 1st session)
-#    return T 
-
-#def normalize_matrix(T):
-#    normT = T/T.sum(axis=0)
-#    return normT
+########################
 
 def sanity_check_matrix():
     T = np.empty((3,3))
@@ -415,26 +412,6 @@ def plot_sanity_check():
     normT = normalize_matrix(T)
     plot_matrix(T,[1,2], k=2,normalized=False)
     plot_matrix(normT,[1,2], k=2)   
- 
-   
-    
-def OLD_compute_distribution(df,session):
-    session_dist = df.query('session_number == @session').groupby('K_cluster').count()[['cell_specimen_id']]
-    cell_count = len(df.cell_specimen_id.unique()) - session_dist['cell_specimen_id'].sum()
-    session_dist.at[K] = [cell_count]
-    session_dist['normalized'] = session_dist['cell_specimen_id']/sum(session_dist['cell_specimen_id'])
-    return session_dist
 
-def compute_full_estimate(df,T, in_session,out_session):
-    in_dist = compute_distribution(df,in_session)
-    out_dist= compute_distribution(df, out_session)
-    out_dist= compute_estimate(T,in_dist, out_dist)
-    plot_estimate_distribution(out_dist)
-
-def compute_estimate(T,in_session_dist,out_session_dist):
-    out_session_dist['estimate_count'] = np.dot(T,in_session_dist['cell_specimen_id'])
-    out_session_dist['estimate_normalized'] = out_session_dist['estimate_count']/sum(out_session_dist['estimate_count'])
-    return out_session_dist
-    
 
 

187a19ecf15c899b1f553b569fec3fd10e0897a8
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-07-06 12:29:44 -0700

    fix missing comma

--- scripts/deploy_glm_fits.sh
+++ scripts/deploy_glm_fits.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source vba
 
-python deploy_glm_fits.py --version 16_events_all_L2_optimize_by_session --env-path /home/dougo/anaconda3/envs/vba --src-path /home/dougo/code/visual_behavior_glm --testing
+python deploy_glm_fits.py --version 17_dff_all_L2_optimize_by_session --env-path /home/dougo/anaconda3/envs/vba --src-path /home/dougo/code/visual_behavior_glm --testing

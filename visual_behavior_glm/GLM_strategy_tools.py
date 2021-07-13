46a1cb5653f38381058999e39b3b39d6b5a751ad
Alex Piet <alexpiet@gmail.com>
2021-07-13 12:45:55 -0700

    binning scatter data, and integrating cre line plots

--- visual_behavior_glm/GLM_strategy_tools.py
+++ visual_behavior_glm/GLM_strategy_tools.py
@@ -47,19 +47,38 @@ def add_behavior_metrics(df):
     out_df['strategy'] = ['visual' if x else 'timing' for x in out_df['visual_strategy_session']]
     return out_df
 
-def scatter_by_session(results_beh, run_params, cre_line=None, threshold=0.01,xmetric='strategy_dropout_index',ymetric='omissions',sessions=[1,3,4,6]):
+def scatter_dataset(results_beh, run_params,threshold=0.01, xmetric='strategy_dropout_index', ymetric='omissions',sessions=[1,3,4,6]):
+    fig, ax = plt.subplots(3,len(sessions)+1, figsize=(18,10))
     fits = {}
-    for s in sessions:
-        fits[str(s)] = scatter_by_cell(results_beh,cre_line=cre_line,xmetric=xmetric, ymetric=ymetric, sessions=[s],title='Session '+str(s))
+    cres = ['Slc17a7-IRES2-Cre', 'Vip-IRES-Cre','Sst-IRES-Cre']
+    ymins = []
+    ymaxs =[]
+    for dex,cre in enumerate(cres):
+        fits[cre] = scatter_by_session(results_beh, run_params, cre_line = cre, threshold=threshold, xmetric=xmetric, ymetric=ymetric, sessions=sessions, ax = ax[dex,:])
+        ymins.append(fits[cre]['yrange'][0])
+        ymaxs.append(fits[cre]['yrange'][1])
     
-    plt.figure()
+    for dex, cre in enumerate(cres):
+        ax[dex,-1].set_ylim(np.min(ymins),np.max(ymaxs))
+    return fits
+
+def scatter_by_session(results_beh, run_params, cre_line=None, threshold=0.01,xmetric='strategy_dropout_index',ymetric='omissions',sessions=[1,3,4,6],ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(1,len(sessions)+1, figsize=(18,3.25))
+
+    fits = {}
+    for dex,s in enumerate(sessions):
+        fits[str(s)] = scatter_by_cell(results_beh,cre_line=cre_line,xmetric=xmetric, ymetric=ymetric, sessions=[s],title='Session '+str(s),ax=ax[dex])
+
+    ax[-1].axhline(0, linestyle='--',color='k',alpha=.25)   
     for s in sessions:
-        plt.plot(s,fits[str(s)][0],'ko')
-        plt.plot([s,s], [fits[str(s)][0]-fits[str(s)][4],fits[str(s)][0]+fits[str(s)][4]], 'k--')
-    plt.ylabel('Regression slope')
-    plt.xlabel('Session Number')  
+        ax[-1].plot(s,fits[str(s)][0],'ko')
+        ax[-1].plot([s,s], [fits[str(s)][0]-fits[str(s)][4],fits[str(s)][0]+fits[str(s)][4]], 'k--')
+    ax[-1].set_ylabel('Regression slope')
+    ax[-1].set_xlabel('Session Number')
     plt.tight_layout()
- 
+
+    fits['yrange'] = ax[-1].get_ylim()
     fits['xmetric'] = xmetric
     fits['ymetric'] = ymetric
     fits['cre_line'] = cre_line
@@ -67,15 +86,34 @@ def scatter_by_session(results_beh, run_params, cre_line=None, threshold=0.01,xm
     fits['glm_version'] = run_params['version'] 
     return fits 
 
-def scatter_by_cell(results_beh, cre_line=None, threshold=0.01, sessions=[1],xmetric='strategy_dropout_index',ymetric='omissions',title=''):
+def scatter_by_cell(results_beh, cre_line=None, threshold=0.01, sessions=[1],xmetric='strategy_dropout_index',ymetric='omissions',title='',nbins=10,ax=None):
     g = results_beh.query('cre_line == @cre_line').query('variance_explained_full > @threshold').query('session_number in @sessions')
 
-    plt.figure()
-    plt.plot(g[xmetric], g[ymetric],'ko')
-    plt.xlabel(xmetric)
-    plt.ylabel(ymetric)
+    # Figure axis
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    
+    # Plot Raw data
+    ax.plot(g[xmetric], g[ymetric],'ko',alpha=.1,label='raw data')
+    ax.set_xlabel(xmetric)
+    ax.set_ylabel(ymetric)
+
+    # Plot binned data
+    g['binned_xmetric'] = pd.cut(g[xmetric],nbins,labels=False) 
+    xpoints = g.groupby('binned_xmetric')[xmetric].mean()
+    ypoints = g.groupby('binned_xmetric')[ymetric].mean()
+    y_sem = g.groupby('binned_xmetric')[ymetric].sem()
+    ax.plot(xpoints, ypoints, 'ro',label='binned data')
+    ax.plot(np.array([xpoints,xpoints]), np.array([ypoints-y_sem,ypoints+y_sem]), 'r-')
+
+    # Plot Linear regression
     x = linregress(g[xmetric], g[ymetric])
-    plt.plot(g[xmetric], x[1]+x[0]*g[xmetric],'r-')
-    plt.title(title)
+    ax.plot(g[xmetric], x[1]+x[0]*g[xmetric],'r-',label='r^2 = '+str(np.round(x.slope,4)))
+
+    # Clean up
+    ax.set_title(title)
+    #ax.legend()
+
     return x    
 

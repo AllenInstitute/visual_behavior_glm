258fb0e94244c4acceb31be3a5a67b0019b68538
Alex Piet <alexpiet@gmail.com>
2021-10-01 10:12:16 -0700

    removing commented out code

--- visual_behavior_glm/glm.py
+++ visual_behavior_glm/glm.py
@@ -138,10 +138,6 @@ class GLM(object):
             self.adj_dropout_summary uses the adjusted dropout and variance explained 
         '''
         self.results = self.gft.build_dataframe_from_dropouts(self.fit)
-        # dropout_summary = gat.generate_results_summary(self)
-        # adj_dropout_summary = gat.generate_results_summary_adj(self)
-        # self.dropout_summary = pd.merge(dropout_summary, adj_dropout_summary,on=['dropout', 'cell_specimen_id']).reset_index()
-        # self.dropout_summary.columns.name = None
         self.dropout_summary = gat.generate_results_summary(self)
 
         # add roi_ids

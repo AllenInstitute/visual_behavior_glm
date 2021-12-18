c98c88464f9f09aa39544fd6436cc2a4757e5090
Alex Piet <alexpiet@gmail.com>
2021-12-17 20:36:37 -0800

    kicking off two additional versions

--- scripts/deploy_glm_fits.py
+++ scripts/deploy_glm_fits.py
@@ -69,7 +69,8 @@ def calculate_required_mem(roi_count):
 def calculate_required_walltime(roi_count):
     '''calculate required walltime in hours'''
     estimate= 10 + 0.125*roi_count
-    return np.min([estimate,24])
+    #return np.min([estimate,24])
+    return 48
 
 def select_experiments_for_testing(returns = 'experiment_ids'):
     '''

9548c00db9357275af19304f085c366bb885ee77
Alex Piet <alexpiet@gmail.com>
2021-12-20 18:02:54 -0800

    starting version with no FA

--- visual_behavior_glm/GLM_params.py
+++ visual_behavior_glm/GLM_params.py
@@ -26,8 +26,8 @@ def define_kernels():
         'hits':         {'event':'hit',         'type':'discrete',      'length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'lick to image change'},
         'misses':       {'event':'miss',        'type':'discrete',      'length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'no lick to image change'},
         'passive_change':   {'event':'passive_change','type':'discrete','length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'passive session image change'},
-        'false_alarms':     {'event':'false_alarm',   'type':'discrete','length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'lick on catch trials'},
-        'correct_rejects':  {'event':'correct_reject','type':'discrete','length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'no lick on catch trials'},
+        #'false_alarms':     {'event':'false_alarm',   'type':'discrete','length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'lick on catch trials'},
+        #'correct_rejects':  {'event':'correct_reject','type':'discrete','length':5.5,   'offset':-1,    'num_weights':None, 'dropout':True, 'text': 'no lick on catch trials'},
         'omissions':        {'event':'omissions',   'type':'discrete',  'length':0.75,  'offset':0,     'num_weights':None, 'dropout':True, 'text': 'image was omitted'},
         'post-omissions':   {'event':'omissions',   'type':'discrete',  'length':1.5,   'offset':0.75,  'num_weights':None, 'dropout':True, 'text': 'images after omission'},
         'each-image':   {'event':'each-image',  'type':'discrete',      'length':0.75,  'offset':0,     'num_weights':None, 'dropout':True, 'text': 'image presentation'},
@@ -170,12 +170,12 @@ def make_run_json(VERSION,label='',username=None, src_path=None, TESTING=False,u
         'experiment_table_path':experiment_table_path,
         'src_file':python_file_full_path,
         'fit_script':python_fit_script,
-        'ElasticNet': True,            # If True, uses the best L1/L2 value for each session
+        'ElasticNet': False,            # If True, uses the best L1/L2 value for each session
         'ElasticNet_nalphas':40,        #
         'ElasticNet_nL1_ratios':40,     #
         'L2_optimize_by_cre': False,    # If True, uses the best L2 value for each cell
         'L2_optimize_by_cell': False,    # If True, uses the best L2 value for each cell
-        'L2_optimize_by_session': False,# If True, uses the best L2 value for this session
+        'L2_optimize_by_session': True, # If True, uses the best L2 value for this session
         'L2_use_fixed_value': False,    # If True, uses the hard coded L2_fixed_lambda  
         'L2_fixed_lambda': None,        # This value is used if L2_use_fixed_value   
         'L2_grid_range':[.1, 500],      # Min/Max L2 values for L2_optimize_by_cell, or L2_optimize_by_session

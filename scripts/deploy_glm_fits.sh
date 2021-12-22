4bd6860e05d181a0ceaf58958e0f524d62e9bb52
Alex Piet <alexpiet@gmail.com>
2021-12-21 18:03:17 -0800

    iterating kernel lengths

--- scripts/deploy_glm_fits.sh
+++ scripts/deploy_glm_fits.sh
@@ -2,4 +2,4 @@
 # Make sure you run conda activate <env> first
 # to run this from an environment where the allenSDK is installed
 
-python deploy_glm_fits.py --version 22_events_all_L2_optimize_by_session_small_licks --env-path /home/alex.piet/codebase/miniconda3/envs/visbeh --src-path /home/alex.piet/codebase/GLM/visual_behavior_glm --job-end-fraction 1 
+python deploy_glm_fits.py --version 22_events_all_L2_optimize_by_session_small_change_3 --env-path /home/alex.piet/codebase/miniconda3/envs/visbeh --src-path /home/alex.piet/codebase/GLM/visual_behavior_glm --job-end-fraction 1 

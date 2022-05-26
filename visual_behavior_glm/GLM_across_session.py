014c19a96209cdcac348a87bdf91a14bc9bf938d
Alex Piet <alexpiet@gmail.com>
2022-05-26 11:27:20 -0700

    working on 15 crashed cells

--- visual_behavior_glm/GLM_across_session.py
+++ visual_behavior_glm/GLM_across_session.py
@@ -19,6 +19,7 @@ def make_across_run_params(glm_version):
     run_params = {}
     run_params['version'] = glm_version+'_across'
     run_params['figure_dir'] = figdir[:-1]
+    run_params['include_4x2_data']=False
     return run_params
 
 def plot_across_summary(across_df,across_run_params,savefig=False):
@@ -172,7 +173,7 @@ def across_session_normalization(cell_specimen_id, glm_version):
     run_params = glm_params.load_run_json(glm_version)
     data = get_across_session_data(run_params,cell_specimen_id)
     score_df = compute_across_session_dropouts(data, run_params, cell_specimen_id)
-    filename = '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/ophys_glm/v_'+glm_version+'/across_session/'+str(cell_specimen_id)+'.csv'
+    filename = '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/ophys_glm/v_'+glm_version+'/across_session/'+str(cell_specimen_id)+'_v2.csv' # TODO DEBUG
     score_df.to_csv(filename)
 
     return data, score_df
@@ -195,8 +196,10 @@ def get_across_session_data(run_params, cell_specimen_id):
     data = {}
     data['ophys_experiment_id'] = oeids
     print('Loading each experiment, will print a bunch of information about the design matrix for each experiment')
+    glm_version = run_params['version']
     for oeid in oeids: 
         print('Loading oeid: '+str(oeid))
+        run_params = glm_params.load_run_json(glm_version) # TODO DEBUG
         session, fit, design = gft.load_fit_experiment(oeid, run_params)       
         data[str(oeid)+'_session'] = session
         data[str(oeid)+'_fit'] = fit
@@ -324,3 +327,15 @@ def append_kernel_excitation_across(weights_df, across_df):
         assert np.all(across_df[kernel+'_across_positive']<=0), "Dropout scores must be negative"
         assert np.all(across_df[kernel+'_across_negative']<=0), "Dropout scores must be negative"
     return across_df
+
+
+def filter_results_pivoted(results_pivoted, across_df):
+    ''' 
+        Filter results_pivoted based on the cells in across_df
+        Just useful for looking at other dropout scores
+    '''
+
+    identifier = across_df['identifier'].unique()
+    results_pivoted_across = results_pivoted.query('identifier in @identifier').copy()
+    return results_pivoted_across
+

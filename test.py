b29552245aece35282d8979922f9aa145491f8a9
Saaketh Medepalli <saaketh.medepalli@alleninstitute.org>
2022-06-11 12:42:49 -0700

    Small change to set upstream branch during pushes

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import visual_behavior_glm.GLM_params as glm_params
 
 print("test")
+
 glm_params.make_run_json("50_Medepalli_test", label='test', username='saaketh.medepalli', src_path='/home/saaketh.medepalli/visual_behavior_glm', TESTING=True) 

823d641a7155888943c126f6820caa2ab6489b9c
Alex Piet <alexpiet@gmail.com>
2022-08-17 15:34:28 -0700

    moving first attempt to bd, then adding data access functions

--- visual_behavior_glm/build_dataframes.py
+++ visual_behavior_glm/build_dataframes.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+import visual_behavior.data_access.loading as loading
+import psy_tools as ps
 
 '''
     Generates two dataframes for each cell
@@ -11,24 +14,73 @@ import pandas as pd
 
 '''
 
+BEHAVIOR_VERSION = 21
 
-def build_response_df(session, cell_specimen_id):
+def load_data(oeid,include_invalid_rois=False):
+    session = loading.get_ophys_dataset(oeid, include_invalid_rois=include_invalid_rois)
+    return session
+
+def load_behavior_summary(session):
+    bsid = session.metadata['behavior_session_id']
+    session_df = ps.load_session_strategy_df(bsid, BEHAVIOR_VERSION)
+    session.behavior_df = session_df 
+
+def build_response_df_cell(session, cell_specimen_id):
     # Interpolate neural activity onto stimulus timestamps
     # 
     # Build response dataframe
     # Save response_df, peak_response_df
-    return full_df
+    return 
 
 def build_response_df_session(session):
+
     # get session level behavior metrics
+    load_behavior_summary(session)
+
     # loop over cells 
+    cell_specimen_ids = session.cell_specimen_table.index.values
+    for index, cell_id in tqdm(enumerate(cell_specimen_ids),
+        total=len(cell_specimen_ids),desc='Iterating Cells'):
+        df = build_response_df_cell(session, cell_id)
         # build_response_df
+
     return
 
 def aggregate_df(df_request, df_type='peak'):
     # df_type should be peak or full
     # df_request should be a df that contains either the cells, or sessions to aggregate (make a choice)
+    return    
     
-    
+### DEV
+################################################################################
+def get_cell_df(session):
+    timestamps = session.ophys_timestamps
+    filtered_event_traces = session.events.iloc[0]['filtered_events']
+    df = pd.DataFrame()
+    df['t'] = timestamps
+    df['response'] = filtered_event_traces
+    return df
 
+def get_cell_eta(df,session,session_df):
+    eta = m.event_triggered_response(
+        data = df,
+        t = 't',
+        y='response',
+        event_times = session.stimulus_presentations.start_time,
+        t_start = 0,
+        t_end = 0.75,
+        output_sampling_rate = 30,
+        interpolate=True
+        )
+    # Add image identity labels
+    eta = pd.merge(eta, session_df,
+        on='stimulus_presentations_id')
+    # add engagement labels
+    # add strategy weights
+    return eta
+
+def get_cell_response(eta,session_df):
+    responses = eta.groupby('stimulus_presentations_id')['response'].max()
+    responses = pd.merge(responses, session_df, on='stimulus_presentations_id')
+    return responses
 
--- visual_behavior_glm/image_regression.py
+++ visual_behavior_glm/image_regression.py
@@ -2,33 +2,4 @@ import numpy as np
 import pandas as pd
 import mindscope_utilities as m
 
-def get_cell_df(session):
-    timestamps = session.ophys_timestamps
-    filtered_event_traces = session.events.iloc[0]['filtered_events']
-    df = pd.DataFrame()
-    df['t'] = timestamps
-    df['response'] = filtered_event_traces
-    return df
 
-def get_cell_eta(df,session,session_df):
-    eta = m.event_triggered_response(
-        data = df,
-        t = 't',
-        y='response',
-        event_times = session.stimulus_presentations.start_time,
-        t_start = 0,
-        t_end = 0.75,
-        output_sampling_rate = 30,
-        interpolate=True
-        )
-    # Add image identity labels
-    eta = pd.merge(eta, session_df,
-        on='stimulus_presentations_id')
-    # add engagement labels
-    # add strategy weights
-    return eta
-
-def get_cell_response(eta,session_df):
-    responses = eta.groupby('stimulus_presentations_id')['response'].max()
-    responses = pd.merge(responses, session_df, on='stimulus_presentations_id')
-    return responses

6b1154fe173fc0da38c0492056a41fffdfa38cac
Alex Piet <alexpiet@gmail.com>
2022-08-29 18:42:37 -0700

    typo

--- scripts/response_dataframes.py
+++ scripts/response_dataframes.py
@@ -13,5 +13,5 @@ parser.add_argument(
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    session = bd.load_data(args.oeid)
+    session = bd.load_data(args.ophys_experiment_id)
     bd.build_response_df_experiment(session)

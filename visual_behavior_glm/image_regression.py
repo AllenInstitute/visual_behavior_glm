034f024ed6a83a52c4e7c69a1e619d014d0578d7
Alex Piet <alexpiet@gmail.com>
2022-09-12 21:21:11 -0700

    setting up image_df QQ

--- visual_behavior_glm/image_regression.py
+++ visual_behavior_glm/image_regression.py
@@ -3,3 +3,97 @@ import pandas as pd
 import mindscope_utilities as m
 
 
+
+def plot_QQ_engagement(image_df,cre,condition,experience_level,savefig=False,quantiles=200,ax=None,
+    data='filtered_events'):
+    
+    # Prep data
+    e_condition = 'engaged_v2_'+condition
+    d_condition = 'disengaged_v2_'+condition
+    df_e = image_df\
+            .query('(condition==@e_condition)&(experience_level==@experience_level)')\
+            .copy()     
+    df_e['max'] = [np.nanmax(x) for x in df_e['response']] 
+
+    df_d = image_df\
+            .query('(condition==@d_condition)&(experience_level==@experience_level)')\
+            .copy()     
+    df_d['max'] = [np.nanmax(x) for x in df_d['response']] 
+ 
+    y = df_e['max'].values
+    x = df_d['max'].values
+    quantiles = np.linspace(start=0,stop=1,num=int(quantiles))
+    x_quantiles = np.nanquantile(x,quantiles, interpolation='linear')[1:-1]
+    y_quantiles = np.nanquantile(y,quantiles, interpolation='linear')[1:-1]
+    max_val = np.max([np.max(x_quantiles),np.max(y_quantiles)])
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.plot(x_quantiles, y_quantiles, 'o-',alpha=.5)
+    ax.plot([0,1.05*max_val],[0,1.05*max_val],'k--',alpha=.5)
+    ax.set_ylabel('engaged quantiles',fontsize=16)
+    ax.set_xlabel('disengage quantiles',fontsize=16)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_tick_params(labelsize=12)
+    ax.yaxis.set_tick_params(labelsize=12)
+    ax.set_aspect('equal')
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(left=0)
+    ax.set_title('{}, {}, {}'.format(cre, condition, experience_level),fontsize=16)
+
+    # Save figure
+    if savefig:
+        filename = PSTH_DIR + data+'/QQ/' +\
+            'QQ_{}_{}_{}.png'.format(cre,condition,experience_level)
+        print('Figure saved to {}'.format(filename))
+        plt.savefig(filename) 
+
+    return ax
+
+
+def plot_QQ_strategy(image_df,cre,condition,experience_level,savefig=False,quantiles=200,ax=None,
+    data='filtered_events'):
+    
+    # Prep data
+    if condition == "omission":
+        df = image_df.query('omitted').copy()
+        #    .query('(omitted)&(experience_level==@experience_level)')\
+        #    .copy()     
+    else:
+        df = image_df.copy()
+    #df['max'] = [np.nanmax(x) for x in df['response']] 
+ 
+    y = df.query('visual_strategy_session')['response'].values
+    x = df.query('not visual_strategy_session')['response'].values
+    quantiles = np.linspace(start=0,stop=1,num=int(quantiles))
+    x_quantiles = np.nanquantile(x,quantiles, interpolation='linear')[1:-1]
+    y_quantiles = np.nanquantile(y,quantiles, interpolation='linear')[1:-1]
+    max_val = np.max([np.max(x_quantiles),np.max(y_quantiles)])
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.plot(x_quantiles, y_quantiles, 'o-',alpha=.5)
+    ax.plot([0,1.05*max_val],[0,1.05*max_val],'k--',alpha=.5)
+    ax.set_ylabel('visual session cell quantiles',fontsize=16)
+    ax.set_xlabel('timing session cell quantiles',fontsize=16)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_tick_params(labelsize=12)
+    ax.yaxis.set_tick_params(labelsize=12)
+    ax.set_aspect('equal')
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(left=0)
+    ax.set_title('{}, {}, {}'.format(cre, condition, experience_level),fontsize=16)
+
+    # Save figure
+    if savefig:
+        filename = PSTH_DIR + data+'/QQ/' +\
+            'QQ_{}_{}_{}.png'.format(cre,condition,experience_level)
+        print('Figure saved to {}'.format(filename))
+        plt.savefig(filename) 
+
+    return ax
+
+
+
+
+

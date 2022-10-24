2735b859c2e2ca335103c582e8036c5b611f8233
Alex Piet <alexpiet@gmail.com>
2022-10-24 11:00:28 -0700

    updating to run dff across session

--- scripts/deploy_across_session.py
+++ scripts/deploy_across_session.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     python_executable = "{}/bin/python".format(args.env_path)
     print('python executable = {}'.format(python_executable))
     python_file = "/home/alex.piet/codebase/GLM/visual_behavior_glm/scripts/across_session.py"
-    glm_version = '24_events_all_L2_optimize_by_session'
+    glm_version = '24_dff_all_L2_optimize_by_session'
     stdout_basedir = "/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/ophys_glm"
     stdout_location = os.path.join(stdout_basedir, 'job_records_across_session')
     if not os.path.exists(stdout_location):

899fe76311224dd5aa9030772d17b2ffd43c871a
Alex Piet <alexpiet@gmail.com>
2022-12-05 15:22:43 -0800

    checking stats for figure 3D across cell types

--- visual_behavior_glm/platform_paper_figure_script.py
+++ visual_behavior_glm/platform_paper_figure_script.py
@@ -70,6 +70,11 @@ gvt.plot_kernel_comparison_by_experience(weights_df, run_params, 'omissions')
 # Returns a dataframe with rows for cre/dropout, and columns describing
 # the dropout score
 stats_D = gvt.plot_dropout_summary_population(results,run_params) 
+results_pivoted_active = results_pivoted.query('not passive').copy()
+anova, tukey = gvt.test_significant_across_cell(results_pivoted_active,'all-images')
+anova, tukey = gvt.test_significant_across_cell(results_pivoted_active,'omissions')
+anova, tukey = gvt.test_significant_across_cell(results_pivoted_active,'behavioral')
+anova, tukey = gvt.test_significant_across_cell(results_pivoted_active,'task')
 
 ## Panel E - Dropout averages for experience and cre-line
 '''

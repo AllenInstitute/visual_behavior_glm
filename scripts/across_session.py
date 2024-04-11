1d699a0ef9c6a21e0a022a8e4cce58337949a7c5
Iryna Yavorska <57956917+yavorska-iryna@users.noreply.github.com>
2024-04-10 23:01:07 -0700

    debugging

--- scripts/across_session.py
+++ scripts/across_session.py
@@ -20,4 +20,4 @@ parser.add_argument(
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    data, score_df = gas.across_session_normalization(args.cell,args.version)
+    data, score_df = gas.across_session_normalization(args.cell,args.version, familiar_only=True)
